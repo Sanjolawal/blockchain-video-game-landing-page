55a5dcb0af1bb7f39d55bb2e0792f138049691bb
sanjo lawal <sanjolawal600@gmail.com>
2022-06-11 04:37:22 -0700

    Copyright div

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../images/logo.svg";
-import dropdown from "../images/dropdown.png" 
-import dot from "../images/dot.svg" 
-import world from "../images/world.png" 
-import close from "../images/close.png" 
+import dropdown from "../images/dropdown.png";
+import dot from "../images/dot.svg";
+import world from "../images/world.png";
+import close from "../images/close.png";
 
 const Navbar = () => {
+  const [value, setvalue] = useState("block");
+
   return (
     <div className="nav">
+      <span
+        className="remove"
+        style={{ display: value }}
+        onClick={() => {
+          setvalue("none");
+        }}
+      >
+        x
+      </span>
+      <a className="all" href="https://wa.link/ibkf23">
+        <p className="WARNING" style={{ display: value }}>
+          SAY NO TO PLAGIARISM/FRAUD
+          <br />
+          <span className="danger">⚠⚠⚠ </span> ACTUAL DEVELOPER FIVERR USERNAME
+          IS FRONTENDEGR OR CLICK ON THIS
+          <a href="https://wa.link/ibkf23" className="wtcolor">
+            LINK
+          </a>
+          TO CHAT DIRECTLY WITH HIM ON
+          <a href="https://wa.link/ibkf23" className="wtcolor">
+            WHATSAPP{" "}
+          </a>
+          <span className="danger"> ⚠⚠⚠ </span>
+        </p>
+      </a>
       <div className="nav2">
         <img src={logo} alt="" className="navimg1" />
         <div className="menu1">
